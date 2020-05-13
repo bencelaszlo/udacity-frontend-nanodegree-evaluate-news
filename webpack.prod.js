bba31d4a05e69f44323e24c0e18d2f951aabdbef
bencelaszlo <bencelaszlo@protonmail.com>
2020-05-13 19:25:39 +0200

    Install and configure optimalization/minimalization plugins to Webpack.
    - Install optimize-css-assets-webpack-plugin, mini-css-extract-plugin.
    - Configure terser-webpack-plugin, clean-webpack-plugin, mini-css-extract-plugin and optimize-css-assets-webpack-plugin for PROD environment.
    
    On branch master
    
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,16 @@
-const path = require('path')
-const webpack = require('webpack')
+// const path = require('path')
+// const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const WorkboxPlugin = require('workbox-webpack-plugin')
+
+// const path = require('path')
+// const webpack = require('webpack')
+// const HtmlWebPackPlugin = require("html-webpack-plugin")
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+// const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -9,6 +18,9 @@ module.exports = {
     output: {
         libraryTarget: 'var',
         library: 'Client'
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
     },
     module: {
         rules: [
@@ -24,10 +36,12 @@ module.exports = {
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(),
+        new WorkboxPlugin.GenerateSW(),
+        new MiniCssExtractPlugin(),
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        }),
-        new WorkboxPlugin.GenerateSW()
+        })
     ]
 }
